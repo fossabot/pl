c759925e24322809eba82a7293ba7ccf1edf50ee
Alexandr Ivanov <aivanov@informatica.ru>
2017-07-12 14:35:14 +0300

    0.2.2 preload

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-function authSuccess() {
-  import ('./helpers/modules')
-    .then(({getModules}) => {
-      return getModules()
-        .then(() => {
-          console.log(2);
-          require('./Root');
-        });
-    });
-}
-console.log(1);
-authSuccess();
+// preload modules.
+const d1 = new Date();
+import ('./helpers/modules')
+  .then(({getModules}) => {
+    return getModules()
+      .then(() => {
+        const d2 = new Date();
+        var timeDiff = Math.abs(d2.getTime() - d1.getTime());
+        console.log('preload time:', timeDiff, 'ms');
+        require('./Root');
+      });
+  });
