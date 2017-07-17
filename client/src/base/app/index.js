511a8a53039ac9297e9d417d5e942b8be1e16932
Alexandr Ivanov <aivanov@informatica.ru>
2017-07-17 12:50:02 +0300

    layout Header, logo

--- client/src/base/app/index.js
+++ client/src/base/app/index.js
@@ -3,7 +3,10 @@ import {Route, Switch} from 'react-router-dom';
 import Main from '../main';
 import Administration from '../administration';
 import {getModuleRoutes} from './../../helpers/routes';
+import {Layout} from 'antd';
 import './../styles/app.css';
+import logo from './logo.png';
+const { Header, Content } = Layout;
 
 class App extends Component {
   constructor (props) {
@@ -16,13 +19,20 @@ class App extends Component {
   render () {
     return (
       <div>
-        <main>
-          <Switch>
-            <Route path="/" component={Main}/>
-            <Route path="/administration" component={Administration}/>
-            {this.state.routes}
-          </Switch>
-        </main>
+        <Layout>
+          <Header>
+            <div className="app-emblem-header">
+              <img src={logo} className="app-emblem"/>
+            </div>
+          </Header>
+          <Content>
+            <Switch>
+              <Route path="/" component={Main}/>
+              <Route path="/administration" component={Administration}/>
+              {this.state.routes}
+            </Switch>
+          </Content>
+        </Layout>
       </div>
     )
   }
