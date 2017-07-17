7232b572ad9d1981a395d697b051c8051d63be49
Alexandr Ivanov <aivanov@informatica.ru>
2017-07-17 12:52:29 +0300

    установил react-redux-leaflet, redux-leaflet-layer

--- client/src/Root.js
+++ client/src/Root.js
@@ -6,7 +6,7 @@ import {ConnectedRouter} from 'react-router-redux';
 import store, {history} from './store';
 import registerServiceWorker from './registerServiceWorker';
 import 'antd/dist/antd.css';
-import './index.css';
+import 'leaflet/dist/leaflet.css';
 
 const target = document.querySelector('#root');
 render(
--- client/src/base/reducers.js
+++ client/src/base/reducers.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux-immutablejs';
 import {routerReducer} from 'react-router-redux';
+import { reducers as lmap } from 'react-redux-leaflet';
+import { reducers as lmapLayer } from 'redux-leaflet-layer';
 import {getModulesProp} from './../helpers/modules';
 
 export default combineReducers({
   routing: routerReducer,
+  ...lmap,
+  ...lmapLayer,
   ...getModulesProp('reducer')
 });
